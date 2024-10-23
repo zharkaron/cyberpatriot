3b5afe37fd7dae3cf08c63071194eaea460a22c3
zharkaron <daron.valdiviaceron@gmail.com>
2024-10-23 15:25:27 -0500

    add user comparison usr.sh

--- usr.sh
+++ usr.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+#Check if the correct number of arguments is provided
+if [ "$#" -ne 1 ]; then
+  echo "usage:$0 <path_to_file>"
+  exit 1
+fi
+
+file_path="$!"
+
+for user in $(grep -Fxv -f <(awk -F: '$3 >= 1000 {print $1}' /etc/passwd) $1); do
+  echo $user
+done
